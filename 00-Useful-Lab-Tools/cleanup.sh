1601faf9b09c4f6ba44123cedfdee5d3bed9e7a9
BrentenDovey-ACG <brenten.dovey@acloud.guru>
2022-02-08 18:27:00 -0600

    Add useful lab tools changes

--- 00-Useful-Lab-Tools/cleanup.sh
+++ 00-Useful-Lab-Tools/cleanup.sh
@@ -6,17 +6,29 @@
 
 ###############################
 ####### SCRIPT DETAILS ########
-# Intended Purpose: Delete all resources in all resource groups available
-# Disclaimer: This script is intended to be used only in the ACG Azure Cloud Playground/Sandbox, be mindful of your subscription context
+# Intended Purpose: Delete all resources in a resource group
+# Disclaimer: This script is intended to be used only in the ACG Azure Cloud Playground/Sandbox
 # Message: Running this script in production environments will create a resume generating event, please be careful
-# Additional Information: I have created an export command to this template in hopes to prevent unrecoverable destruction, should you accidentally misuse this script
-# Exported Template Location: The scripts will be exported for each resource group to your local working directory
+# Additional Information: This script will export resources to ARM templates in hopes to prevent unrecoverable destruction, should you accidentally misuse this script
+# Exported Template Location: The resource templates will be exported for each resource to your local working directory
 ###############################
 
+##############################
+##### START - VARIABLES ######
+##############################
 
 # Set variables
 rg=`az group list --query '[].name' -o tsv`
 location=`az group list --query '[].location' -o tsv`
+
+##############################
+##### END - VARIABLES ######
+##############################
+
+
+##############################
+####### START - SCRIPT #######
+##############################
 
 # Iterate through all resources from a resource group defined by our rg variable
 for resource in `az group list-resources --resource-group $rg --query '[].name' -o tsv`
@@ -27,3 +39,7 @@ do
     # Export resource as an ARM template
     az resource export --resource-group $rg --name $resource --query 'properties.outputs' -o tsv > $resource.json
 done
+
+##############################
+######## END - SCRIPT ########
+##############################

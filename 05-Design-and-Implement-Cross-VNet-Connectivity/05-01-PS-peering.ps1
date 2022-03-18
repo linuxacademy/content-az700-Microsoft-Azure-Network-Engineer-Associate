42534a2045e9fc2feed0b7d65a44da9dd0332ede
Matthew Ulasien <38888227+mattuLA@users.noreply.github.com>
2022-03-18 14:01:42 -0500

    Create 05-01-PS-peering.ps1

---
+++ 05-Design-and-Implement-Cross-VNet-Connectivity/05-01-PS-peering.ps1
@@ -0,0 +1,18 @@
+$HubVnet = Get-AzVirtualNetwork -Name "cake-hub-vnet"
+$Spoke2Vnet = Get-AzVirtualNetwork -Name "cake-spoke2-vnet"
+
+Add-AzVirtualNetworkPeering -Name 'hub-spoke2' `
+    -VirtualNetwork $HubVnet `
+    -RemoteVirtualNetworkId $Spoke2Vnet.Id `
+    -AllowForwardedTraffic `
+    -AllowGatewayTransit
+
+# Note: the spoke2-hub peering will fail because the hub network does not have a gateway to peer to.
+# We are leaving the command as is for learning purposes, 
+# however you will need to remove the 'UseRemoteGateways' open to successfully run it.
+
+Add-AzVirtualNetworkPeering -Name 'spoke2-hub' `
+    -VirtualNetwork $Spoke2Vnet `
+    -RemoteVirtualNetworkId $HubVnet.Id `
+    -AllowForwardedTraffic `
+    -UseRemoteGateways
